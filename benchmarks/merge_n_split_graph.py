0340ff07aedb1a2ff0114dc5609fdbdceee0f27d
lucia <5190890+lcipolina@users.noreply.github.com>
2024-05-19 23:09:12 +0200

    graphs for the merge and split

--- benchmarks/merge_n_split_graph.py
+++ benchmarks/merge_n_split_graph.py
@@ -23,11 +23,13 @@ def compute_merge_and_split_costs(n_agents):
     total_costs = [m + s for m, s in zip(merge_costs, split_costs)]
     return total_costs
 
+
+# PLOT
 def plot_costs(n_agents, total_costs, constant_cost_value=1):
     """Plot the computational costs with specified parameters."""
     constant_costs = [constant_cost_value] * len(n_agents)
 
-    plt.figure(figsize=(7, 6)) # to make it same as the costs graphs
+    plt.figure(figsize=(7, 6)) # to make it same size as the costs graphs
     plt.plot(n_agents, total_costs, label='Total Cost (Merge and Split)', marker='o', color='blue')
     plt.plot(n_agents, constant_costs, label='DRL (ours) (O(1) Method)', marker='x', color='red')
 
@@ -39,9 +41,11 @@ def plot_costs(n_agents, total_costs, constant_cost_value=1):
     plt.title('Computational Costs Between Merge and Split and Ours')
     plt.legend()
     plt.grid(True)
-    plt.savefig('merge_n_split_comp_costs1.png')
+    plt.savefig('merge_n_split_comp_costs.png')
     plt.show()
 
+
+############################################ RUNNER ############################################
 def main(n_agents):
     """Main entry point for calculating and plotting computational costs."""
     # Compute costs for the merge and split method
